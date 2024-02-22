1940aac23f572f582baaa40ab673f01e16059a2c
Filipp Riabchun <filipp.riabchun@jetbrains.com>
2024-02-22 21:37:36 +0100

    init

---
+++ dist/src_dynamic_js.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkwebpack_css_order"] = self["webpackChunkwebpack_css_order"] || []).push([["src_dynamic_js"],{
+
+/***/ "./src/dynamic.js":
+/*!************************!*\
+  !*** ./src/dynamic.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _b_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./b.css */ \"./src/b.css\");\n/* harmony import */ var _a_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./a.css */ \"./src/a.css\");\n\n\n\n\n//# sourceURL=webpack://webpack-css-order/./src/dynamic.js?");
+
+/***/ })
+
+}]);
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mode: 'development',
+    experiments: {
+        css: true
+    }
+}
